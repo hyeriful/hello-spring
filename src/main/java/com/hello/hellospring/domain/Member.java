5c6b514f6e780966459b0f49ee6bf270a29c3e50
hyeriful <dlgpfl2523@gmail.com>
2020-11-10 20:52:31 +0900

    JPA entity mapping

--- src/main/java/com/hello/hellospring/domain/Member.java
+++ src/main/java/com/hello/hellospring/domain/Member.java
@@ -1,6 +1,16 @@
 package com.hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // 이제, Member는 JPA가 관리하는 entity
 public class Member {
+
+    // PK mapping
+    // DB에서 pk(id)를 자동으로 생성해주고 있음 - identity
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
